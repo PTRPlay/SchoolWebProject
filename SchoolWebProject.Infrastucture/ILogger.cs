2f682add564636bba233f841dec9dbd9bbb77e19
CodyExho <humenetskyymaryan@gmail.com>
2015-10-26 09:40:58 +0200

    LVNETO-12. Logger implementation.

---
+++ SchoolWebProject.Infrastucture/ILogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolWebProject.Infrastucture
+{
+    private interface ILogger
+    {
+        void WarningLog(string message);
+
+        void InfoLog(string message);
+
+        void ErrorLog(string message);
+        
+        void DebugLog(string message);
+        
+        void TraceLog(string message);
+    }
+}
---
+++ SchoolWebProject.Infrastucture/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace SchoolWebProject.Infrastucture
+{
+    public class Logger : ILogger
+    {
+        public void WarningLog(string message)
+        {
+            Debug.Write(message);
+        }
+
+        public void InfoLog(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        public void ErrorLog(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        public void DebugLog(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        public void TraceLog(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
