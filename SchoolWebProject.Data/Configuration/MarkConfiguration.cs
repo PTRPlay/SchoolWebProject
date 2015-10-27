2d06dfabaf7473c0606db0bdd835ed1a0eaf8fc6
unknown <vilnui@gmail.com>
2015-10-27 13:29:54 +0200

    Add SchoolWebProject.Data.

---
+++ SchoolWebProject.Data/Configuration/MarkConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SchoolWebProject.Domain.Models;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolWebProject.Data.Configuration
+{
+    class MarkConfiguration : EntityTypeConfiguration<Mark>
+    {
+        public MarkConfiguration()
+        {
+            this.ToTable("Mark");
+        }
+    }
+}
