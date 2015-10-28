b76d6fc7da64a5c5f6b83f123a65cca5d65a6bdf
Mahotc <Magotskuy@gmail.com>
2015-10-28 09:07:13 +0300

    Rename test project and put him into SchoolWebProject.Services.Tests folder

--- SchoolWebProject.Domain.Models/Announcement.cs
+++ SchoolWebProject.Domain.Models/Announcement.cs
@@ -16,7 +16,6 @@ namespace SchoolWebProject.Domain.Models
         [Required]
         [MaxLength(200)]
         public string Message { get; set; }
-        [MaxLength(3000)]
         public string MessageDetails { get; set; }
 
         public int UserId { get; set; }
