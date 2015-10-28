55caf12d0d132556a3da5eaa513cf01d07463c4e
ymahotc <mahotskuy@gmail.com>
2015-10-28 22:08:21 +0300

    Change namespace of TestProject. Also create new class TeatcherServiseUnitTest and add to there a test for all mathod in TetcherService class

--- SchoolWebProject.Services.Tests/UnitTest/UnitTest/Properties/AssemblyInfo.cs
+++ SchoolWebProject.Services.Tests/UnitTest/UnitTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyTitle("UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Win-Yagd")]
-[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyProduct("UnitTest")]
 [assembly: AssemblyCopyright("Copyright © Win-Yagd 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5fda1bf4-7171-465a-80e3-dcf1c9e96d36")]
+[assembly: Guid("1ccb3113-da57-4432-9ec5-70ae8035888a")]
 
 // Version information for an assembly consists of the following four values:
 //
