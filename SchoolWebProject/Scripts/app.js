9db713c59fbe6509e0f5b83fd1d1d46d76c9b97b
MykolaKlymenko <klymenko8@gmail.com>
2015-11-01 01:00:34 +0200

    i worked with _Layout and create app.js with myApp module. i had some problem with routing (i can't use temlateUrl so i have decided to use  <scripts text/ng-context> , I NEED HELP with solving trouble with templateUrl). i used ngRouting (i know ui.Routing is better).

---
+++ SchoolWebProject/Scripts/app.js
@@ -0,0 +1,40 @@
+﻿var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+
+        .when('/teachers', {
+            templateUrl: 'Teachers.html',
+            controller: 'teachersController'
+        })
+
+        .when('/subjects', {
+            templateUrl:'Subjects.html',
+            controller: 'subjectsController'
+        })
+
+        .when('/scheldule', {
+            template: 'Scheldule.html',
+            controller: 'shelduleController'
+        })
+        .when('/news', {
+            template: 'News.html',
+            controller: 'newsController'
+        });
+});
+
+myApp.controller('teachersController', function ($scope,$http) {
+    
+});
+
+myApp.controller('subjectsController', function ($scope) {
+
+    });
+
+myApp.controller('schelduleController', function ($scope) {
+    //there will be some stuff
+});
+
+myApp.controller('newsController', function ($scope) {
+    //there will be some stuff
+});
