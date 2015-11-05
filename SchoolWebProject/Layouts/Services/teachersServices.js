ea6a9bcd6630be1efb2e018ef4bae65715c69e39
unknown <vilnui@gmail.com>
2015-11-05 16:13:08 +0200

    Viewing teacher list

--- SchoolWebProject/Layouts/Services/teachersServices.js
+++ SchoolWebProject/Layouts/Services/teachersServices.js
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
 myApp.factory('teachers', ['$http', function ($http){
-	return $http.get('api/teachers')
+	return $http.get('api/teacher')
 	.success(function (data) {
 		return data.get;
 	})
 	.error (function (data) {
 	    return data;
 	})
-=======
-﻿myApp.factory('teachers', ['$http', function ($http) {
-<<<<<<< HEAD
-    return $http.get('Home/GetTeachers')
-       .success(function (data) {
-           return data;
-       })
-       .error(function (data) {
-           return data;
-       });
->>>>>>> 6cbb462dd287b01d260ba7bcd93ba068bcac2345
-=======
-    return $http.get('api/teacher').success(function (data) {
-        return data;
-    });
 
->>>>>>> 6f82f1eb14c683198fa2abf6dab902a332c1e5c7
-}]);
+﻿}]);
