80c11bdcaac5b7c186d52b5ec25a5cff6b5bf5d8
HalynaVladyka <halyna.vladyka@gmail.com>
2015-11-05 14:27:38 +0200

    Add PupilService, AnnouncementService

---
+++ SchoolWebProject.Services/AnnouncementService.cs
@@ -0,0 +1,55 @@
+﻿using SchoolWebProject.Data.Infrastructure;
+using SchoolWebProject.Domain.Models;
+using SchoolWebProject.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolWebProject.Services
+{
+    public class AnnouncementService : BaseService, IAnnouncementService
+    {
+        private IRepository<Announcement> repository;
+
+        private IUnitOfWork unitOfWork;
+
+        public AnnouncementService(ILogger logger, IRepository<Announcement> announcementRepository, IUnitOfWork unitOfWork)
+            : base(logger)
+        {
+            repository = announcementRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Announcement> GetAllAnnouncements()
+        {
+            return repository.GetAll();
+        }
+
+        public Announcement GetAnnouncementById(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        public void UpdateAnnouncement(Announcement announcement)
+        {
+            repository.Update(announcement);
+        }
+
+        public void AddAnnouncement(Announcement announcement)
+        {
+            repository.Add(announcement);
+        }
+
+        public void RemoveAnnouncement(Announcement announcement)
+        {
+            repository.Delete(announcement);
+        }
+
+        public void SaveAnnouncement()
+        {
+            unitOfWork.SaveChanges();
+        }
+    }
+}
