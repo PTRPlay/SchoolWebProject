90aee3aa87ed40b457f7a6b29f3538c2197ded3e
PTRPlay <trs7taras@gmail.com>
2015-11-07 19:56:56 +0200

    Created modal window for pupils

---
+++ SchoolWebProject/Layouts/ModalControllers/pupilModal.js
@@ -0,0 +1,17 @@
+﻿myApp.controller('ModalShowController', ['$scope', 'ModalService', '$http', function ($scope, ModalService, $http) {
+    $scope.showTeachersEditPage = function (user) {
+        ModalService.showModal({
+            templateUrl: "Layouts/PupilAddTemplate.html",
+            controller: "teacherAddController",
+            inputs: {
+                title: "Вчитель",
+                Teacher: user
+            }
+        }).then(function (modal) {
+            modal.element.modal();
+            modal.close.then(function (result) {
+                $http.post("api/teacher", result);
+            });
+        });
+    };
+}]);
