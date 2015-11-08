40955dcd8b241c186ed74cfa0c497ffd656dc252
PTRPlay <trs7taras@gmail.com>
2015-11-08 18:45:30 +0200

    Pupil modal window bug fix

--- SchoolWebProject/Layouts/ModalControllers/Modal.js
+++ SchoolWebProject/Layouts/ModalControllers/Modal.js
@@ -14,4 +14,20 @@
             });
         });
     };
+
+    $scope.showPupilsEditPage = function (user) {
+        ModalService.showModal({
+            templateUrl: "Layouts/PupilAddTemplate.html",
+            controller: "pupilAddController",
+            inputs: {
+                title: "Учень",
+                Pupil: user
+            }
+        }).then(function (modal) {
+            modal.element.modal();
+            modal.close.then(function (result) {
+                $http.post("api/pupil", result);
+            });
+        });
+    };
 }]);
