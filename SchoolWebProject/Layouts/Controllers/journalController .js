eb20fb6836546d09b4f47a302f00d52b2dae8dc4
CodyExho <humenetskyymaryan@gmail.com>
2015-11-18 22:05:50 +0200

    Add pupil object with array of marks in journalController

--- SchoolWebProject/Layouts/Controllers/journalController .js
+++ SchoolWebProject/Layouts/Controllers/journalController .js
@@ -17,6 +17,13 @@ myApp.controller('journalController', ['$scope', 'markService','subjects','group
        pinnedLeft: true,
        enableCellEdit: false,
        enableFiltering: false,
+   },
+   {
+       field: "firstName",
+       width: "100",
+       pinnedLeft: false,
+       enableCellEdit: false,
+       enableFiltering: false
    }
 
         ],
@@ -26,11 +33,45 @@ myApp.controller('journalController', ['$scope', 'markService','subjects','group
 
     };
 
+    var pupilsMarks = [];
+
+    var AddPupil = function (Pupil) {
+        pupilsMarks.push({
+            id: Pupil.Id,
+            firstName: Pupil.FirstName,
+            lastName: Pupil.LastName
+        });
+    }
+
+    var fillPupils = function () {
+        for (var i = 0; i < data.Pupils.length; ++i) {
+            AddPupil(data.Pupils[i]);
+        }
+        for (var i = 0; i < pupilsMarks.length; ++i){
+            for (var j = 0; j < data.LessonDetails.length; ++j){
+                pupilsMarks[i][data.LessonDetails[j].Date.toString()] = null;
+            }
+        }
+    }
+
+    var fillMarks = function () {
+        fillPupils();
+        for (var i = 0; i < pupilsMarks.length; ++i) {
+            for (var j = 0; j < data.Marks.length; ++j) {
+                if (data.Marks.Pupil.FirstName == pupilsMarks[i].firstName && data.Marks.Pupil.LastName == pupilsMarks[i].LastName) {
+                    pupilsMarks[i][data.Marks.LessonDetail.Date.toString()] = data.Marks.Value;
+                }
+            }
+        }
+    }
+
+    fillMarks();
+
     $scope.GetJournalPage = function (chosenSubject,chosenGroup)
     {
         if (chosenGroup != null && chosenSubject != null)
             markService.getPage(chosenSubject, chosenGroup).success(function (data) {
-                $scope.journalGrid.data = data;
+                $scope.data = data;
             });
     }
 
