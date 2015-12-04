6a4e432412dbc657e91b97da4da01072847898a3
YuraMahotc <mahotskuy@gmail.com>
2015-12-04 11:45:32 +0300

    Add chackBox and some function to edit Schedule

--- SchoolWebProject/Layouts/Controllers/scheduleController.js
+++ SchoolWebProject/Layouts/Controllers/scheduleController.js
@@ -1,6 +1,16 @@
-﻿myApp.controller('scheduleController',['$scope','$http','scheduleService',function ($scope,$http,scheduleService) {
+﻿var some = 2;
+var isShowScheduleInvoked = false;
+function GetList(id) {
+    if (document.getElementById('editCheckBox').checked && isShowScheduleInvoked)
+        alert(id);
+}
+
+myApp.controller('scheduleController', ['$scope', '$http', 'scheduleService', function ($scope, $http, scheduleService) {
     scheduleService.InitializeAutocomplate();
+    $scope.IsValidUser = window.currentUser.Role == "Admin";
+    $scope.IsEnableEdit = false;
     $scope.showSchedule = function () {
+        isShowScheduleInvoked = true;
         var teacherFilter = (document.getElementById('TeacherFilter').value).replace(/\s+/g, '');
         var subjectFilter = (document.getElementById('SubjectFilter').value).replace(/\s+/g+/-/, '');
         if (teacherFilter == "") teacherFilter ="null";
@@ -9,4 +19,5 @@
         if (subjectFilter == "") filter += "null";
         var fullSchedule = scheduleService.loadSchedule(filter)
     }
+    
 }]);
