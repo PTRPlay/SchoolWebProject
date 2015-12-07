ed5132380bb8d4fd90eea7f7f9846db72801090f
unknown <ymahotc@CLASS04.training.local>
2015-12-07 12:12:53 +0200

    Add function to generate LessonDetail in LessonDetailService. Delete SaveChange from MarkController and add in service Rename MarkController .cs to MarkController.cs

--- SchoolWebProject.Services/LessonDetailService.cs
+++ SchoolWebProject.Services/LessonDetailService.cs
@@ -32,6 +32,65 @@ namespace SchoolWebProject.Services
         public void UpdateLessonDetail(LessonDetail lessonDetail)
         {
             this.unitOfWork.LessonDetailRepository.Update(lessonDetail);
+            SaveLessonDetail();
+        }
+
+        public void GenereteLessonDeatail(Schedule addedSchedule)
+        {
+            var holidays = unitOfWork.HolidaysRepository.GetAll();
+            var IsExistLessonDetalis = this.unitOfWork.LessonDetailRepository.GetMany(p => p.ScheduleId == addedSchedule.Id);
+            DateTime DataOfLesson;
+            DateTime StartTerm = new DateTime(2015, 09, 1);
+            DateTime EndTerm = new DateTime(2015, 12, 30);
+            DateTime FirstDateOfLesson=StartTerm;
+            LessonDetail NewLessonDetail;
+                do
+                {
+                    if ((int)StartTerm.DayOfWeek == addedSchedule.DayOfTheWeek)
+                    {
+                        FirstDateOfLesson = StartTerm;
+                        break;
+                    }
+                    else
+                    {
+                        StartTerm = StartTerm.AddDays(1);
+                    }
+                } while (StartTerm < EndTerm);
+               
+            if (IsExistLessonDetalis.Count() == 0)
+            {
+                DataOfLesson = FirstDateOfLesson;
+                do
+                {
+                    bool IsLessonDateInHolidays = false;
+                    
+                    foreach (var holiday in holidays)
+                    {
+                        if (DataOfLesson > holiday.StartDay && DataOfLesson < holiday.EndDay)
+                        {
+                            IsLessonDateInHolidays = true;
+                            break;
+                        }
+                    }
+                    if(!IsLessonDateInHolidays)
+                    {
+                        NewLessonDetail = new LessonDetail { ScheduleId = addedSchedule.Id, Date = DataOfLesson, SchoolId = 1 };
+                        this.unitOfWork.LessonDetailRepository.Add(NewLessonDetail);
+                        SaveLessonDetail();
+                    }
+                    DataOfLesson = DataOfLesson.AddDays(7);
+                } while (DataOfLesson <= EndTerm);
+            }
+            else
+            {
+                foreach (var lessonDetail in IsExistLessonDetalis)
+                {
+                    lessonDetail.ScheduleId = addedSchedule.Id;
+                    UpdateLessonDetail(lessonDetail);
+                    SaveLessonDetail();
+                }
+                
+            }
         }
 
         public void AddLessonDetail(LessonDetail lessonDetail)
